2f20131bc75a22a75015550611b4886c73f55f74
aodSaid <aodsaidou17331@gmail.com>
2022-04-22 17:51:20 +0200

    mise à jour 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import ReactDOM from 'react-dom'; 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import'./style/styles.scss'
--- src/pages/Knowledges.js
+++ src/pages/Knowledges.js
@@ -4,7 +4,6 @@ import Hobbies from '../components/knowledges/Hobbies';
 import Languages from '../components/knowledges/Languages';
 import Navigation from '../components/Navigation';
 import OtherSkills from '../components/knowledges/OtherSkills';
-import { ParamKeyValuePair } from 'react-router-dom';
 import Parcours from '../components/knowledges/Parcours';
 const Knowledges = () => {
     return (
